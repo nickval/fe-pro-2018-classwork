5b87b79e10724f0a70ebe811534da625faee58a9
Jan10 <nickval@ukr.net>
2018-05-31 21:11:23 +0300

    lection20

---
+++ lection20/main.js
@@ -0,0 +1,67 @@
+console.log("server started123");
+
+// var menu = [];
+
+
+function doAjax(method, file) {
+
+    var promise = new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open(method, file, true);
+        xhr.send();
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) {
+                return;
+            }
+            console.log('resolve');
+            // resolve(JSON.parse(xhr.responseText));
+
+            if (xhr.status != 200 || xhr.responseText == '{}') {
+                reject(xhr);
+            } else {
+                resolve(JSON.parse(xhr.responseText));
+            }
+        }
+    });
+
+    return promise;
+};
+
+// var req1 = doAjax('GET', '/data.json');
+// var req2 = doAjax('GET', '/data2.json');
+
+// Promise.all([req1, req2])
+//     .then(function (response) {
+//         console.log(response);
+
+//         // return response;
+//     })
+//     .catch(function (response) {
+//         console.log(response);
+//     });
+
+
+function getMenu(role) {
+    return doAjax('GET', `/${role}.json`)
+}
+
+window.onload = function () {
+    var menu = document.getElementsByClassName('menu');
+    console.log(menu);
+    getMenu('user')
+        .then(function (response) {
+            // menuArray = response.menu;
+            console.log(response.menu);
+            // for (let i = 0; i < response.menu.length; i++) {
+            //     console.log(i);
+            //     var div = document.createElement("div");
+        
+            //     // div.classList.add("menu-item");
+            //     div.innerHTML = response[i].menu;
+            //     menu.appendChild(div);
+            // }
+        });
+    
+}
+
