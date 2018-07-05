71a353c3119ee199ec9203072a49c8b3f19d0f72
Jan10 <nickval@ukr.net>
2018-07-05 18:51:29 +0300

    lect27

--- lection27/app.js
+++ lection27/app.js
@@ -3,15 +3,15 @@ let url = 'https://api.github.com/orgs/hillel-front-end/repos',
     reposList;
 
 data.doAjax('GET', url)
-            // .then(reposList.map(list) => Promise.apply(list))
-            .then(function (values) {
-                // console.log('app', values);
-                var reposList = values.map(function(name) {
-                    console.log(name.forks_url);
-                    return name.forks_url;
-                });
-                return executeRequest(reposList);
-            })
+            .then(reposList => Promise.apply(list => map(list.forks_url)))
+            // .then(function (values) {
+            //     // console.log('app', values);
+            //     var reposList = values.map(function(name) {
+            //         console.log(name.forks_url);
+            //         return name.forks_url;
+            //     });
+            //     return executeRequest(reposList);
+            // })
             .then(list => Promise.all(list))
             .then(res => console.log(res));
 
