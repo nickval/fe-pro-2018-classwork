815f115812170959a29ca9d40c5e6147535c3728
Jan10 <nickval@ukr.net>
2018-07-05 21:03:42 +0300

    mock server

---
+++ lection29/03_mock/routes/data.js
@@ -0,0 +1,79 @@
+let fs = require('fs');
+let path = '.';
+
+function pathConcat(pathname) {
+	return path + '/' + pathname;
+}
+
+function filereader(fsRef, path) {
+	return new Promise(function (resolve, reject) {
+		fsRef.readFile(path, 'utf8', function (e, d) {
+
+			if (e) reject(e);
+
+			else resolve(JSON.parse(d));
+		});
+	});
+}
+
+function getBuildings(req, res) {
+	let path = pathConcat('api' + req.route.path + '/' + req.method.toLowerCase() + '.json'),
+		servicePromise = filereader(fs, path);
+
+	console.log(path);
+	
+	servicePromise
+		.then((response) => {
+			// let newList = response.results.filter(item => item.price);
+
+			// response.results = newList;
+
+			return response;
+		})
+		.then((response) => {
+			res.json(response);
+		});
+}
+
+function getUser(req, res) {
+	let path = pathConcat('api' + req.url + '/' + req.method.toLowerCase() + '.json'),
+		servicePromise = filereader(fs, path);
+
+	console.log(path);
+	
+	servicePromise
+		.then((response) => {
+			if (!response.success) {
+				let info = {
+					user : {
+						"status": "blocked"
+					}
+				};
+
+				res.json(info);
+
+				return Promise.reject()
+			} 
+			
+
+			return response;
+		})
+		.then((response) => {
+			res.json(response);
+		});
+}
+
+function list(req, res) {
+	let path = pathConcat('api' + req.url + '/' + req.method.toLowerCase() + '.json'),
+		servicePromise = filereader(fs, path);
+
+	console.log(path);
+	
+	servicePromise
+		.then((response) => {
+			res.json(response);
+		});
+}
+
+
+module.exports = { getBuildings, pathConcat, getUser, list };
